99ee24d64a0feed20257b5265de46639a5cdfb43
Jungmin, Kim <jmkim7707@gmail.com>
2025-07-20 22:38:11 +0900

    commit

--- axlrator_start.sh
+++ axlrator_start.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
+LOG_DIR=/app/logs
+mkdir -p "$LOG_DIR"
+
 # 날짜 형식
 DATE=$(date +"%Y%m%d_%H%M%S")
 
 echo "[Frontend] Starting..."
-cd "$BASE_DIR" || exit
-nohup npm run dev -- --host 0.0.0.0 > "$LOG_DIR/frontend_$(date +'%Y%m%d_%H%M%S').log" 2>&1 &
-cd "$BASE_DIR" || exit
+cd /app/axlrator-webui
+nohup npm run preview -- --host 0.0.0.0 > "/app/logs/frontend_$(date +'%Y%m%d_%H%M%S').log" 2>&1 &
 
 echo "[Backend] Starting..."
-/app/axlrator-webui/backend/start.sh
-
-
-# 백엔드 포어그라운드 실행
-echo "[Backend] Starting..."
-/app/axlrator-webui/backend/start.sh
+cd /app/axlrator-webui/backend
+./start.sh
 
